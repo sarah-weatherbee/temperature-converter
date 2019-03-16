44162f68aaef639c9cc520ffb8adff1ab82fa948
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-03-16 16:06:30 -0500

    added logic to check for number in input, added clear fxn

--- main.js
+++ main.js
@@ -14,23 +14,37 @@ const toFahrenheit = (temp) => {
    domStringBuilder(finalF, "F");
  }
 
+const clear = () => {
+    let input = document.getElementById("tempInput");
+    input.value = "";
+    let output =document.getElementById("tempOutput");
+    output.innerHTML = "";
+}
+
 const domStringBuilder = (finalTemp, unit) => {
     const domString = `<h2>${finalTemp} degrees ${unit}</h2>`
     printToDom("tempOutput", domString);
 }
 
 const determineConverter = (e) => {
-    const inputValue = document.getElementById("tempInput").value;
-    if (document.getElementById("C").checked) {
-        toCelcius (inputValue) 
-    }else {
-        toFahrenheit (inputValue)
+    let inputValue = document.getElementById("tempInput").value;
+    if (inputValue.length === 0 || isNaN(Number(inputValue))) {
+        alert("type a number, please")
+        clear()
     }
+    else {
+        if (document.getElementById("C").checked) {
+            toCelcius (inputValue) 
+        }else {
+            toFahrenheit (inputValue)
+        }
+    }
+    
   }
-
 
 const buttonEvents = () => {
     document.getElementById('convertBtn').addEventListener('click', determineConverter);
+    document.getElementById('clearBtn').addEventListener('click', clear);
 };
 
 const init = () => {
